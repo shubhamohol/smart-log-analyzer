625b1b7690eb22288133855b88e45df18cc30f01
YourGitHubUsername <your@email.com>
2026-08-26 18:49:00 +0530

    new Datttaa

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,27 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 23773
+    port: Number(process.env.DB_PORT) || 23773,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+// Test database connection
+db.getConnection((err, connection) => {
     if (err) {
-        console.log("Database connection failed:", err.message);
+        console.error("Database connection failed:", err.message);
         return;
     }
 
     console.log("MySQL Database Connected!");
+
+    connection.release();
 });
 
 module.exports = db;
